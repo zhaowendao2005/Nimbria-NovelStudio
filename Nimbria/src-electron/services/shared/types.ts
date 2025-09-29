6097da6893db82669b9e748eeaf4c35390b0847b
zhaowendao2005 <140528990+zhaowendao2005@users.noreply.github.com>
2025-09-29 16:25:09 +0800

    完成需求分析工作流

--- Nimbria/src-electron/services/shared/types.ts
+++
@@ -1,9 +0,0 @@
-/**
- * 服务共享类型定义
- */
-
-// TODO: 定义服务共享类型
-// eslint-disable-next-line @typescript-eslint/no-empty-object-type
-export interface ServiceConfig {
-  // 预留服务配置基础类型
-}
--- Nimbria/src-electron/services/webview-service/cookie-manager.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Cookie管理器
- */
-
-// TODO: 实现Cookie管理功能
-export class CookieManager {
-  // 预留Cookie管理功能
-}
